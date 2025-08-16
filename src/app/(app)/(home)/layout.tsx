ad81c3891c2a5dec931eef2234e1a1bd3cdeabb4
sifat1234 <ahahmedsifat111@gmail.com>
2025-08-16 17:19:11 +0600

    display each category & subcategory

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,15 +1,41 @@
 import { Footer } from '@/modules/home/ui/components/footer';
 import { Navbar } from '@/modules/home/ui/components/navbar';
+import { SearchFilters } from '@/modules/home/ui/components/search-filters';
+import { Category } from '@/payload-types';
+import configPromise from '@payload-config';
+import { getPayload } from 'payload';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout = async ({ children }: Props) => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: 'categories',
+    pagination: false,
+    depth: 1,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      ...(doc as Category),
+    })),
+  }));
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar />
-
+      <SearchFilters data={formattedData} />
       <div className='flex-1 bg-[#F4F4F0]'>{children}</div>
       <Footer />
     </div>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,23 +1,4 @@
-import configPromise from '@payload-config';
-import { getPayload } from 'payload';
-
 const Page = async () => {
-  const payload = await getPayload({
-    config: configPromise,
-  });
-
-  const data = await payload.find({
-    collection: 'categories',
-    depth: 1,
-    where: {
-      parent: {
-        exists: false,
-      },
-    },
-  });
-
-  console.log(data);
-
   return <div>page</div>;
 };
 
