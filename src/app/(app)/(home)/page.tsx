0570dc3ca890b57cd97deb26715dd6f528dc3568
sifat1234 <ahahmedsifat111@gmail.com>
2025-08-16 16:07:41 +0600

    enhance categories collection

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,4 +1,23 @@
-const Page = () => {
+import configPromise from '@payload-config';
+import { getPayload } from 'payload';
+
+const Page = async () => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: 'categories',
+    depth: 1,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
+  console.log(data);
+
   return <div>page</div>;
 };
 
