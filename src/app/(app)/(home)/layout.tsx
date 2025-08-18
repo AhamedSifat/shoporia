3916c9c39a23f96f0e747341023ed5c6984546cf
sifat1234 <ahahmedsifat111@gmail.com>
2025-08-18 14:48:41 +0600

    prefetch categories in layout.tsx

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,43 +1,25 @@
 import { Footer } from '@/modules/home/ui/components/footer';
 import { Navbar } from '@/modules/home/ui/components/navbar';
 import { SearchFilters } from '@/modules/home/ui/components/search-filters';
-import { Category } from '@/payload-types';
-import { CustomCategory } from '@/types';
-import configPromise from '@payload-config';
-import { getPayload } from 'payload';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout = async ({ children }: Props) => {
-  const payload = await getPayload({
-    config: configPromise,
-  });
-
-  const data = await payload.find({
-    collection: 'categories',
-    pagination: false,
-    depth: 1,
-    where: {
-      parent: {
-        exists: false,
-      },
-    },
-    sort: 'name',
-  });
-
-  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
-    ...doc,
-    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-      ...(doc as Category),
-    })),
-  }));
-
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.categories.getMany.queryOptions());
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar />
-      <SearchFilters data={formattedData} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchFilters />
+        </Suspense>
+      </HydrationBoundary>
       <div className='flex-1 bg-[#F4F4F0]'>{children}</div>
       <Footer />
     </div>
--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -1,12 +1,12 @@
-import { CustomCategory } from '@/types';
+'use client';
 import { Categories } from './categories';
 import { SearchInput } from './search-input';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { useTRPC } from '@/trpc/client';
 
-interface Props {
-  data: CustomCategory[];
-}
-
-export const SearchFilters = ({ data }: Props) => {
+export const SearchFilters = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
   return (
     <div className='px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full'>
       <SearchInput data={data} />
