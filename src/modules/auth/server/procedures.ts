6f4982931c8e4ed577d0657a7c7f5b95d0ce2543
sifat1234 <ahahmedsifat111@gmail.com>
2025-08-18 16:21:37 +0600

    add login mutation with cookie-based session handling

--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -1,7 +1,8 @@
 import { baseProcedure, createTRPCRouter } from '@/trpc/init';
 
-import { headers as getHeaders } from 'next/headers';
-import { registerSchema } from '../schema';
+import { headers as getHeaders, cookies as getCookies } from 'next/headers';
+import { loginSchema, registerSchema } from '../schema';
+import { TRPCError } from '@trpc/server';
 
 export const authRouter = createTRPCRouter({
   session: baseProcedure.query(async ({ ctx }) => {
@@ -22,5 +23,59 @@ export const authRouter = createTRPCRouter({
           password: input.password,
         },
       });
+
+      const data = await ctx.payload.login({
+        collection: 'users',
+        data: {
+          email: input.email,
+          password: input.password,
+        },
+      });
+
+      if (!data.token) {
+        throw new TRPCError({
+          code: 'UNAUTHORIZED',
+          message: 'failed to login',
+        });
+      }
+
+      const cookies = await getCookies();
+      cookies.set({
+        name: 'AUTH_COOKIE',
+        value: data.token,
+        httpOnly: true,
+        path: '/',
+        // sameSite: 'none',
+        // domain:
+      });
     }),
+
+  login: baseProcedure.input(loginSchema).mutation(async ({ input, ctx }) => {
+    const data = await ctx.payload.login({
+      collection: 'users',
+      data: {
+        email: input.email,
+        password: input.password,
+      },
+    });
+
+    if (!data.token) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: 'failed to login',
+      });
+    }
+
+    const cookies = await getCookies();
+    cookies.set({
+      name: 'AUTH_COOKIE',
+      value: data.token,
+      httpOnly: true,
+      path: '/',
+      // sameSite: 'none',
+      // domain:
+    });
+
+    return data;
+  }),
 });
