6e114583d11aa3918aff6c451cf13366654c8f46
sifat1234 <ahahmedsifat111@gmail.com>
2025-08-26 18:25:41 +0600

    add Prodcuts Collection

---
+++ src/collections/Products.ts
@@ -0,0 +1,46 @@
+import type { CollectionConfig } from 'payload';
+
+export const Products: CollectionConfig = {
+  slug: 'products',
+
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+
+    {
+      name: 'description',
+      type: 'text',
+      required: true,
+    },
+
+    {
+      name: 'price',
+      type: 'number',
+      required: true,
+    },
+
+    {
+      name: 'category',
+      type: 'relationship',
+      relationTo: 'categories',
+      hasMany: false,
+    },
+
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      hasMany: false,
+    },
+
+    {
+      name: 'refundPolicy',
+      type: 'select',
+      options: ['30 days', '60 days', '90 days', 'no-refunds'],
+      defaultValue: '30 days',
+    },
+  ],
+};
